0a9809f46f07622a8af0541b21d870fe83ca4462
allysonlister <github@lurena.com>
2022-10-05 10:42:51 +0100

    Cleaning up directory structure to prepare for merge with master.

--- seed-via-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-ODK_IMAGE=${ODK_IMAGE:-odkfull}
-ODK_TAG=${ODK_TAG:-latest}
-
-docker run -v $HOME/.gitconfig:/root/.gitconfig -v $PWD:/work -w /work --rm obolibrary/$ODK_IMAGE:$ODK_TAG /tools/odk.py seed "$@"
